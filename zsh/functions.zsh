32c82aefa1f6a9137cce2c1d5a07da488664671b
Brad Gignac <bgignac@bradgignac.com>
2015-01-14 17:17:37 -0500

    Add Go path to work function.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,5 @@
 work() {
-  cd $(find ~/work -maxdepth 2 -type d | selecta)
+  cd $({ find ~/work -maxdepth 2 -type d ; find ~/work/go/src -maxdepth 3 -type d } | sort | uniq | selecta)
 }
 
 search() {
